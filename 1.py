254e2bd55aa558841f47e23ef5a45d8c872d9193
Lennoxgonz <75677560+Lennoxgonz@users.noreply.github.com>
2021-08-25 12:02:02 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 #A String
 class_name = "P5_Delinquents: " 
 #error class_name +
-student1 = "Carlos "
+student1 = "Carlos, "
 top_delinquents = student1 + "Nickolas, Gianna, Gonzalo"
 class_students = class_name + top_delinquents
 
@@ -15,12 +15,12 @@ print(student2)
 student3 = "Carlos" + str(third_student)
 print(student3)
 
-grade1 = (input("What did you get in P1?"))
-grade2 = (input("What did you get in P2?"))
-grade3 = (input("What did you get in P3?"))
-grade4 = (input("What did you get in P4?"))
+grade1 = int(input("What did you get in P1?"))
+grade2 = int(input("What did you get in P2?"))
+grade3 = int(input("What did you get in P3?"))
+grade4 = int(input("What did you get in P4?"))
 
-print("To verify: You scored " + grade1 + "ln" + grade2 + grade3 + grade4)
+str(print("To verify: You scored " + grade1 + "ln" + grade2 + "ln" + grade3 + "ln" + grade4)) 
 
-gpa = (grade1 + grade2 + grade3 + grade4)
+gpa = (grade1 + grade2 + grade3 + grade4) / 4
 print(gpa)
